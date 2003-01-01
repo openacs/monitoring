83e7362a7e8f0f86a0c555b4ce7174ec6f3711a0
Vinod Kurup <vinod@kurup.org>
2003-01-01 08:36:41 +0000

    - Fixed display to work for daily and weekly scheduled procedures

--- www/scheduled-procs.tcl
+++ www/scheduled-procs.tcl
@@ -36,15 +36,62 @@ set time_fmt "%m-%d %T"
 set counter 0
 set bgcolors { white #E0E0E0 }
 
-proc ad_scheduled_procs_compare { a b } {
-    set next_run_a [expr { [lindex $a 5] + [lindex $a 2] }]
-    set next_run_b [expr { [lindex $b 5] + [lindex $b 2] }]
-    if { $next_run_a < $next_run_b } {
-    return -1
-    } elseif { $next_run_a > $next_run_b } {
-    return 1
+proc ad_scheduled_procs_nextrun { interval last_run } {
+    #  if simple ns_schedule_proc, interval will be integer
+    #  if ns_schedule_daily, interval will be "hour" and "min" of next run
+    #  if ns_schedule_weekly, interval will be "day", "hour" and "min"
+
+    if { [llength $interval] == 1 } {
+        set next_run [expr { $last_run + $interval }]
+    } elseif { [llength $interval] == 2 } {
+        set hour [lindex $interval 0]
+        set minute [lindex $interval 1]
+
+        set next_run [clock scan "${hour}:${minute}"]
+        
+        # has it already run today? Then get tomorrow's value
+        if { [clock seconds] > $next_run } {
+            set next_run [expr $next_run + 86400]
+        }
+    } elseif { [llength $interval] == 3 } {
+        set day_num [lindex $interval 0]
+        switch $day_num {
+            0 { set day "Sunday" }
+            1 { set day "Monday" }
+            2 { set day "Tuesday" }
+            3 { set day "Wednesday" }
+            4 { set day "Thursday" }
+            5 { set day "Friday" }
+            6 { set day "Saturday" }
+        }
+        set hour [lindex $interval 1]
+        set minute [lindex $interval 2]
+        set next_run [clock scan "$day ${hour}:${minute}"]
+
+        # has it already run this week? Then get next week's value
+        if { [clock seconds] > $next_run } {
+            set next_run [expr $next_run + 604800]
+        }
     } else {
-    return [string compare [lindex $a 3] [lindex $b 3]]
+        ad_return_error "Error: Unknown interval" \
+            "Error in monitoring/www/scheduled-procs.tcl : 
+             ad_scheduled_procs_nextrun"
+    }
+
+    return $next_run
+}
+
+proc ad_scheduled_procs_compare { a b } {
+    # compare based on when next run is scheduled
+    set next_run_a [ad_scheduled_procs_nextrun [lindex $a 2] [lindex $a 5]]
+    set next_run_b [ad_scheduled_procs_nextrun [lindex $b 2] [lindex $b 5]]
+
+    if { $next_run_a < $next_run_b } {
+        return -1
+    } elseif { $next_run_a > $next_run_b } {
+        return 1
+    } else {
+        return [string compare [lindex $a 3] [lindex $b 3]]
     }
 }
 
@@ -58,26 +105,26 @@ foreach proc_info [lsort -command ad_scheduled_procs_compare [nsv_get ad_procs .
     set proc [lindex $proc_info 3]
     set args [lindex $proc_info 4]
     if { $args == "" } {
-    set args "&nbsp;"
+        set args "&nbsp;"
     }
     set time [lindex $proc_info 5]
     set count [lindex $proc_info 6]
     set debug [ad_decode [lindex $proc_info 7] "t" "Yes" "No"]
     set last_run [ad_decode $count 0 "&nbsp;" [ns_fmttime $time $time_fmt]]
-    set next_run [ns_fmttime [expr { $time + $interval }] $time_fmt]
-    set next_run_in "[expr { $time + $interval - [ns_time] }] s"
+    set next_run [ns_fmttime [ad_scheduled_procs_nextrun $interval $time] $time_fmt]
+    set next_run_in "[expr { [ad_scheduled_procs_nextrun $interval $time] - [ns_time] }] s"
 
     append page_content "<tr>"
     foreach name { proc args } {
-    append page_content "<td bgcolor=$bgcolor>[set $name]</td>"
+        append page_content "<td bgcolor=$bgcolor>[set $name]</td>"
     }
     append page_content "<td bgcolor=$bgcolor align=right>$count</td>"
     foreach name { last_run next_run } {
-    append page_content "<td bgcolor=$bgcolor>[set $name]</td>"
+        append page_content "<td bgcolor=$bgcolor>[set $name]</td>"
     }
     append page_content "<td bgcolor=$bgcolor align=right>$next_run_in</td>"
     foreach name { thread once debug } {
-    append page_content "<td bgcolor=$bgcolor>[set $name]</td>"
+        append page_content "<td bgcolor=$bgcolor>[set $name]</td>"
     }
     append page_content "</tr>\n"
 }
