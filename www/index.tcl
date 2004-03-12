5abc6431fda2114dd83f0479dd2fad1c47c554cf
Joel Aufrecht <joel@aufrecht.org>
2004-03-12 13:57:24 +0000

    changed index page to use adp; added link to parameters

--- www/index.tcl
+++ www/index.tcl
@@ -1,8 +1,9 @@
-
 ad_page_contract {
     Show all the links to Monitoring sub-modules
     @cvs-id $Id$
 } {}
+
+set package_id [ad_conn package_id]
 
 set dev_support_link ""
 set dev_support_id [apm_package_id_from_key "acs-developer-support"]
@@ -13,35 +14,13 @@ if { $dev_support_id != 0 } {
     }
 }
 
-# vinodk: FIXME: show only those links specific to the running database
+set parameters_url [export_vars -base "/shared/parameters" {
+    package_id { return_url [ad_return_url] }
+}]
 
-doc_return 200 text/html "[ad_header "Monitoring  [ad_system_name]"]
+set context [list]
+set title "Monitoring [ad_system_name]"
 
-<h2>Monitoring  [ad_system_name]</h2>
-
-[ad_context_bar]
-
-<hr>
-
-<ul>
-
-<li><a href=\"monitor\">Current page requests</a>
-<li><a href=\"cassandracle\">Cassandracle</a> (Oracle)
-<li><a href=\"configuration\">Configuration</a>
-<li><a href=\"watchdog\">WatchDog</a> (Error Log)
-(log messages written from within PL/SQL)
-<li><a href=\"filters\">Filters</a>
-<li><a href=\"registered-procs\">Registered Procedures</a>
-<li><a href=\"scheduled-procs\">Scheduled Procedures</a>
-<li><a href=\"startup-log\">Startup Log</a>
-<li><a href=\"top\">Statistics from top</a>
-<li><a href=\"analyze\">Table analysis</a>
-$dev_support_link
-
-</ul>
-
-[ad_admin_footer]
-"
-
+ad_return_template
 
 
